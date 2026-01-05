8dad3780e93232f41894a16e4b5981217b527487
Catalina Aguilar <catalina.aguilar@seidor.com>
2026-01-05 04:00:55 -0300

    se implementa cambio entre oscuro y claro y avance en home

--- src/app/components/navbar-global/navbar-global.ts
+++ src/app/components/navbar-global/navbar-global.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink, RouterModule } from "@angular/router";
 
 @Component({
   selector: 'ix-navbar-global',
-  imports: [RouterLink],
+  imports: [RouterLink, RouterModule, MatIconModule],
   templateUrl: './navbar-global.html',
   styleUrl: './navbar-global.scss',
 })
 export class NavbarGlobal {
+  isDark = true;
+
+  toggleTheme(){
+    this.isDark = !this.isDark;
+    document.body.setAttribute(
+      'data-theme',
+      this.isDark? 'dark' : 'light'
+    );
+  }
 
 }

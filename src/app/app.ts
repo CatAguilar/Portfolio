b0942f90e87409127573031953c263df18cdb6d2
Catalina Aguilar <catalina.aguilar@seidor.com>
2026-01-05 00:22:55 -0300

    se agregan paginas rutas y navbar simple

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarGlobal } from './components/navbar-global/navbar-global';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarGlobal],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/components/navbar-global/navbar-global.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+
+@Component({
+  selector: 'ix-navbar-global',
+  imports: [RouterLink],
+  templateUrl: './navbar-global.html',
+  styleUrl: './navbar-global.scss',
+})
+export class NavbarGlobal {
+
+}
